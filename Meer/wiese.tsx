d9bc9439a3eeee67469383a032c7009b0377a864
brombeere <nadelohr0815@web.de>
2021-02-13 20:19:52 +0100

    copy tile

---
+++ Meer/wiese.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="wiese" tilewidth="32" tileheight="32" tilecount="5082" columns="77">
+ <image source="../kueste/https___af-oio_maps_at_rc3_world__background.png" width="2464" height="2112"/>
+</tileset>
